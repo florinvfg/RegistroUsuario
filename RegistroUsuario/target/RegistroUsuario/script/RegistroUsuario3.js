e84c7274c21f4c36dd4c0702eb80251297d7934a
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2024-05-28 17:24:47 +0200

    Add files via upload

--- RegistroUsuario/target/RegistroUsuario/script/RegistroUsuario3.js
+++ RegistroUsuario/target/RegistroUsuario/script/RegistroUsuario3.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     form.nombre.addEventListener('input', validarNombre);//elemento input->dentro del input,se va a la funccion validar nombre
     form.apellido.addEventListener('input', validarApellido);
+    form.edad.addEventListener('input', validarEdad);
+    form.telefono.addEventListener('input', validarTelefono);
+    form.email.addEventListener('input', validarEmail);
 });
 
 function validarNombre() {//validamos que no este vacio el nombre
@@ -49,8 +52,73 @@ function validarApellido() {//validamos que no este vacio el nombre
     }
 }
 
+function validarEdad(){
+    let edad = document.querySelector('#edad');
+    let msnError = document.querySelector('.edadError');
+
+    if (!edad.value.trim() || isNaN(edad.value)){
+        edad.classList.add("errorInput");
+        edad.classList.remove('validoInput');
+        msnError.textContent = 'El edad es obligatorio.';
+        return false;
+    }else if (edad.value<=0){
+        edad.classList.add('errorInput');
+        edad.classList.remove('validoInput');
+        msnError.textContent = 'La edad debe ser un numero positivo';
+        return false;
+    }else {
+        edad.classList.remove('error-input');
+        edad.classList.add('validoInput');
+        msnError.textContent = '';
+        return true;
+    }
+}
+function validarTelefono() {
+    let telefono = document.querySelector('#telefono');
+    let msnError = document.querySelector('.telefonoError');
+
+    if (!telefono.value.trim() || isNaN(telefono.value)) {
+        telefono.classList.add("errorInput");
+        telefono.classList.remove('validoInput');
+        msnError.textContent = 'El telefono es obligatorio.';
+        return false;
+    }
+    else if (!telefono.value.match(/^[6789]\d{8}$/)){
+        telefono.classList.add('errorInput');
+        telefono.classList.remove('validoInput');
+        msnError.textContent = 'El telefono debe tener 9 numeros y comensar por 6/7/8/9';
+        return false;
+    }else {
+        telefono.classList.remove('error-input');
+        telefono.classList.add('validoInput');
+        msnError.textContent = '';
+        return true;
+    }
+}
+function validarEmail() {
+    let email = document.querySelector('#email');
+    let msnError = document.querySelector('.emailError');
+
+    if (!email.value.trim() || isNaN(email.value)) {
+        email.classList.add("errorInput");
+        email.classList.remove('validoInput');
+        msnError.textContent = 'El email es obligatorio.';
+        return false;
+    }
+    else if (!email.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/)){
+        email.classList.add('errorInput');
+        email.classList.remove('validoInput');
+        msnError.textContent = 'El email no es en formato corecto';
+        return false;
+    }else {
+        email.classList.remove('error-input');
+        email.classList.add('validoInput');
+        msnError.textContent = '';
+        return true;
+    }
+}
 function validarFomulario() {
-    if (validarNombre() && validarApellido())
+    if (validarNombre() && validarApellido() && validarEdad() && validarTelefono() && validarEmail())
         return true;
     else
         return false;
