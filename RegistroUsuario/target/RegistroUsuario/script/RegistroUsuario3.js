2071c675966f085aeae9c88c3f479ec1a3b8daa6
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2024-05-28 16:24:14 +0200

    Add files via upload

--- RegistroUsuario/target/RegistroUsuario/script/RegistroUsuario3.js
+++ RegistroUsuario/target/RegistroUsuario/script/RegistroUsuario3.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let form = document.querySelector('#registroForm');
 
     form.nombre.addEventListener('input', validarNombre);//elemento input->dentro del input,se va a la funccion validar nombre
-
+    form.apellido.addEventListener('input', validarApellido);
 });
 
 function validarNombre() {//validamos que no este vacio el nombre
@@ -27,9 +27,30 @@ function validarNombre() {//validamos que no este vacio el nombre
     }
 }
 
+function validarApellido() {//validamos que no este vacio el nombre
+    let apellido = document.querySelector('#apellido');
+    let msnError = document.querySelector('.apellidoError');
+
+    if (!apellido.value.trim()) {
+        apellido.classList.add("errorInput");
+        apellido.classList.remove('validoInput');
+        msnError.textContent = 'El apellido es obligatorio.';
+        return false;
+    } else if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(apellido.value.trim())) {//solo letra y espacios
+        apellido.classList.add('errorInput');
+        apellido.classList.remove('validoInput');
+        msnError.textContent = 'El apellido solo puede contener letras y espacios.';
+        return false;
+    } else {//esta bien,y lo ponemos en verde
+        apellido.classList.remove('error-input');
+        apellido.classList.add('validoInput');
+        msnError.textContent = '';
+        return true;
+    }
+}
 
 function validarFomulario() {
-    if (validarNombre())
+    if (validarNombre() && validarApellido())
         return true;
     else
         return false;
